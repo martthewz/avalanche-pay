32ed1c95bf8636b52510a56964a6867a32198388
Matthew Okusimba <matthewokusimba@gmail.com>
2020-12-10 16:20:48 +0300

    Initial Commit

---
+++ php-sdk/src/AvalanchePay/Api/Transaction.php
@@ -0,0 +1,30 @@
+<?php 
+namespace PayMoney\Api;
+
+use PayMoney\Common\PayMoneyModel;
+
+/**
+ * Class Transaction
+ * @property \PayMoney\Api\Amount amount
+ *
+ */
+
+class Transaction extends PayMoneyModel
+{
+
+    /**
+     * @param \PayMoney\Api\Amount $amount
+     *
+     * @return $this
+     */
+    public function setAmount($amount)
+    {
+        $this->amount = $amount;
+        return $this;
+    }
+
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+}
